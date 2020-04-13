0c5c6a2c7ddff3daa41bb4948dea02efb3e070cd
Pilmo Kang <ivorycirrus@gmail.com>
2020-04-13 23:02:56 +0900

    add templete sources

---
+++ ssp_java/src/ssp_01/P4SortAggregation.java
@@ -0,0 +1,9 @@
+package ssp_01;
+
+public class P4SortAggregation {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ ssp_java/src/ssp_01/P6Example.java
@@ -0,0 +1,9 @@
+package ssp_01;
+
+public class P6Example {
+
+	public static void main(String[] args) {
+
+	}
+
+}
